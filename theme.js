001dbc292d98449723fec6c14587d1dc27426256
Stormy <80485413+0hStormy@users.noreply.github.com>
2025-07-16 19:06:13 -0700

    Make theme a seperate file

---
+++ theme.js
@@ -0,0 +1,14 @@
+const exampleCustomTheme = {
+    "sidebar": "#111",
+    "nav": "#111",
+    "bg": "#000",
+    "box": "#111",
+    "sec": "#ffffff11",
+    "accent": "#2484b1ff",
+    "text": "#ebebeb",
+
+    "round": "0px",
+    "font": "Segoe UI"
+}
+
+hintApplyTheme(exampleCustomTheme)
